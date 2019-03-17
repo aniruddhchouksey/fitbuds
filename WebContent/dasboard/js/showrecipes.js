527df4ed5d955be2c5e6dcf5f6814bf50eb05955
Sahaj23 <Sahaj23@Sahaj>
2019-03-17 12:16:02 +0530

    Customize diet proper functioning

--- WebContent/dasboard/js/showrecipes.js
+++ WebContent/dasboard/js/showrecipes.js
@@ -1,4 +1,4 @@
- $(document).ready(function () {
+        $(document).ready(function () {
     var data = JSON.parse(window.localStorage.getItem('data'));
     var name = window.localStorage.getItem('name');
     var i;
